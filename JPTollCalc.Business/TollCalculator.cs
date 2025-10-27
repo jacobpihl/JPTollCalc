d2fbee196992435f773aa35fe1954854f248c8e1
jacobpihl <pihl.jacob@gmail.com>
2025-10-27 17:41:46 +0100

    Improve holiday check

--- JPTollCalc.Business/TollCalculator.cs
+++ JPTollCalc.Business/TollCalculator.cs
@@ -1,4 +1,5 @@
 ﻿using JPTollCalc.Contracts;
+using PublicHoliday;
 
 namespace JPTollCalc.Business;
 
@@ -85,26 +86,9 @@ public class TollCalculator
 
     private static bool IsHolidayDate(DateTime date)
     {
-        var year = date.Year;
-        var month = date.Month;
-        var day = date.Day;
-        
-        if (year == 2013)
-        {
-            if (month == 1 && day == 1 ||
-                month == 3 && (day == 28 || day == 29) ||
-                month == 4 && (day == 1 || day == 30) ||
-                month == 5 && (day == 1 || day == 8 || day == 9) ||
-                month == 6 && (day == 5 || day == 6 || day == 21) ||
-                month == 7 ||
-                month == 11 && day == 1 ||
-                month == 12 && (day == 24 || day == 25 || day == 26 || day == 31))
-            {
-                return true;
-            }
-        }
-        
-        return false;
+        // TODO: Probably use a trusted source for the holidays rather than a random Nuget package :)
+        var holidayChecker = new SwedenPublicHoliday();
+        return holidayChecker.IsPublicHoliday(date);
     }
     
     private bool IsTollFreeVehicle(IVehicle vehicle)
